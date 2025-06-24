40b62a937b09499c789dbbb36006edbc9ddc6e1a
neutrixinc <153314273+neutrixinc@users.noreply.github.com>
2025-06-25 00:38:02 +0600

    Add tailwind.config.ts 1750790281864

---
+++ tailwind.config.ts
@@ -0,0 +1,64 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        '3xl': '1792px',
+        '4xl': '1920px',
+      },
+      colors: {
+        primary: '#34A203',
+        body_text: '#1F2A37',
+        body_text2: '#4B5563',
+      },
+      padding: {
+        '2-percent': 'calc(100vw*0.02)',
+        '3-percent': 'calc(100vw*0.03)',
+        '4-percent': 'calc(100vw*0.04)',
+        '5-percent': 'calc(100vw*0.05)',
+        '6-percent': 'calc(100vw*0.06)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
+}
+export default config
